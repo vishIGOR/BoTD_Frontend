2520e131fae8b26803e29e4cfca1924f4b29d1bb
vishIGOR <vish_ig3@mail.ru>
2025-03-15 02:16:47 +0700

    fix import problem

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,17 @@
+import "antd/dist/reset.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "./pages/Main.tsx";
-import CreateRequestPage from "./pages/CreateRequestPage.tsx";
-import LoginPage from "./pages/Login.tsx";
-import RegisterPage from "./pages/Register.tsx";
 import "./index.css";
-import "antd/dist/reset.css";
+import LoginPage from "./pages/Login.tsx";
+import Main from "./pages/Main.tsx";
+import RegisterPage from "./pages/Register.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/create-request" element={<CreateRequestPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
